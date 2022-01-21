3cda12fa687091d59d307a0ae8cfbcc10d5d2d03
Jean-Hadrien Chabran <jh@chabran.fr>
2022-01-21 18:04:01 +0100

    woops

--- enterprise/dev/ci/internal/ci/asdf_operation.go
+++ enterprise/dev/ci/internal/ci/asdf_operation.go
@@ -7,17 +7,17 @@ import (
 
 func ASDFInstall() []bk.StepOpt {
 	return []bk.StepOpt{
-		buildkite.Env("AWS_CONFIG_FILE", "/buildkite/.aws/config"),
-		buildkite.Env("AWS_SHARED_CREDENTIALS_FILE", "/buildkite/.aws/credentials"),
-		buildkite.Cache(&buildkite.CacheOptions{
-			ID:          "asdf",
-			Key:         "cache-asdf-{{ checksum '.tool-versions' }}",
-			RestoreKeys: []string{"cache-asdf-{{ checksum '.tool-versions' }}"},
-			Paths:       []string{".buildkite-cache/asdf/downloads"},
-		}),
-		buildkite.Cmd("ls ~/.asdf/downloads/"),
-		buildkite.Cmd("ls .buildkite-cache/asdf/downloads/"),
-		buildkite.Cmd("asdf list"),
+		// buildkite.Env("AWS_CONFIG_FILE", "/buildkite/.aws/config"),
+		// buildkite.Env("AWS_SHARED_CREDENTIALS_FILE", "/buildkite/.aws/credentials"),
+		// buildkite.Cache(&buildkite.CacheOptions{
+		// 	ID:          "asdf",
+		// 	Key:         "cache-asdf-{{ checksum '.tool-versions' }}",
+		// 	RestoreKeys: []string{"cache-asdf-{{ checksum '.tool-versions' }}"},
+		// 	Paths:       []string{".buildkite-cache/asdf/downloads"},
+		// }),
+		// buildkite.Cmd("ls ~/.asdf/downloads/"),
+		// buildkite.Cmd("ls .buildkite-cache/asdf/downloads/"),
+		// buildkite.Cmd("asdf list"),
 		buildkite.Cmd("asdf install"),
 	}
 }
